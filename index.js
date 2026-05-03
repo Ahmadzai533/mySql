4729205eee7dd40414c624e1530027b35016a429
Roohullah Ahmadzai <roohullahpoohan@gmail.com>
2026-05-03 19:19:19 +0430

    PS C:\Users\Roohullah Ahmdzai\Desktop\mySql> git log --oneline 6a3d93c (HEAD -> main, origin/main, origin/HEAD) updated bda2b2d updated af6ba47 updated 9f2ed50 updated d9fd1d7 updated 677a67d updated 476926c updated 8995351 updated 15b6001 updated 941b794 updated :

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const mysql = require("mysql2");
 
 const express = require("express");
 const app = express();
-// const path = require("path");
 
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "/views"));
+const { v4: uuidv4 } = require("uuid");
 
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -58,10 +63,77 @@ app.get("/users", (req, res) => {
 
 app.get("/users/:id/edit", (req, res) => {
   let { id } = req.params;
-  let q = `select * from users where id = ${id}`;
-  res.render("edit.ejs");
+  let q = `select * from users where id = '${id}'`;
+});
+
+app.patch("/users/:id", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT *FROM users WHERE id='${id}'`;
+  let { password: formpass, username: newUsername } = req.body;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let users = result[0];
+      if (formPass != users.password) res.send("wrong password");
+      else {
+        let q2 = `UPDATE users SET username='${newUsername}' WHERE id='${id}'`;
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          res.redirect(result);
+        });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    console.log("Error inserting data into the database");
+  }
+});
+
+app.get("/users/new", (req, res) => {
+  res.render("adduser.ejs");
+});
+
+app.post("/users/new", (req, res) => {
+  let { username, email, password } = req.body;
+  let id = uuidv4();
+  //Query to Insert New User
+  let q = `INSERT INTO users  (id, username, email, password) VALUES ('${id}','${username}','${email}','${password}') `;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      console.log("added new user");
+      res.redirect("/users");
+    });
+  } catch (err) {
+    res.send("some error occurred");
+  }
 });
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
+
+// app.patch("/users/:id", (req, res) => {
+//   let { id } = req.params;
+//   let { password: formpass, username: newUsername } = req.body;
+//   let q = `select * from users where id = '${id}'`;
+//   try {
+//     connection.query(q, (err, result) => {
+//       if (err) throw err;
+//       let users = result[0];
+//       if (formpass != users.password) res.send("Wrong password");
+//       else {
+//         let q2 = `UPDATE users SET username='${newUsername}' WHERE id='${id}'`;
+//         connection.query(q2, (err, result) => {
+//           if (err) throw err;
+//           res.redirect(result);
+//         });
+//       }
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     console.log("Error inserting data into the database");
+//   }
+// });
